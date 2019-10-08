44d9949cb29c2779dfe5fd4e3d47790e4d9244e9
Vicente Insfran <vinsfran@gmail.com>
2019-10-08 14:57:29 -0300

    Cambios en deudas

--- src/app/components/modulos/deudas/deudas.model.ts
+++ src/app/components/modulos/deudas/deudas.model.ts
@@ -4,6 +4,7 @@ import {ServiciosModel} from '../servicios/servicios.model';
 export class DeudasModel {
   servicio_id: number;
   factura: string;
+  importe: number;
   vencimiento: Date;
   estado: string;
   cliente: ClienteModel;
